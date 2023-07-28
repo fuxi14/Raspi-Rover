b8632ee3cf7c38638c81d8bb687e2e6e4299319e
fuxi14 <76251359+fuxi14@users.noreply.github.com>
2023-07-28 19:36:48 +0200

    Implemented arrow buttons

--- src/project/Rovercontroller/app/src/main/java/local/mahouse/rovercontroller/ui/slideshow/SlideshowFragment.java
+++ src/project/Rovercontroller/app/src/main/java/local/mahouse/rovercontroller/ui/slideshow/SlideshowFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.arch.lifecycle.ViewModelProvider;
+
+import pl.droidsonroids.gif.GifImageView;
 
 import local.mahouse.rovercontroller.R;
 
@@ -19,8 +21,46 @@ public class SlideshowFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View slideshowFragment = inflater.inflate(R.layout.fragment_slideshow, container, false);//You f*ucking idiot
 
-        final TextView textView = slideshowFragment.findViewById(R.id.text_slideshow);
-        textView.setText("This is the slideshow fragment");
+
+
+        //Gif que indica la direcció
+        GifImageView direction = (GifImageView) slideshowFragment.findViewById(R.id.gifDirection);
+
+        ImageButton btnForward = (ImageButton) slideshowFragment.findViewById(R.id.btnForward);
+        ImageButton btnReverse = (ImageButton) slideshowFragment.findViewById(R.id.btnReverse);
+        ImageButton btnLeft = (ImageButton) slideshowFragment.findViewById(R.id.btnLeft);
+        ImageButton btnRight = (ImageButton) slideshowFragment.findViewById(R.id.btnRight);
+
+        btnForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                direction.setRotation(-90);
+            }
+        });
+
+        btnReverse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                direction.setRotation(90);
+            }
+        });
+
+        btnLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                direction.setRotation(180);
+            }
+        });
+
+        btnRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                direction.setRotation(0);
+            }
+        });
+
+
+        //NO TOCAR
         return slideshowFragment;
     }
 
