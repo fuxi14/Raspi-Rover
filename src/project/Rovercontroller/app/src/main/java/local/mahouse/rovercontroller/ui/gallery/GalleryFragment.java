7d65a5c4e1a3d6ddc5a6abf89af60c70d16be658
fuxi14 <76251359+fuxi14@users.noreply.github.com>
2023-08-11 20:33:34 +0200

    Applied Joystick view and started to handle position to translate to movement and speed

--- src/project/Rovercontroller/app/src/main/java/local/mahouse/rovercontroller/ui/gallery/GalleryFragment.java
+++ src/project/Rovercontroller/app/src/main/java/local/mahouse/rovercontroller/ui/gallery/GalleryFragment.java
@@ -4,77 +4,116 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
+
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.widget.Toast;
 
-import java.util.ArrayList;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.zerokol.views.joystickView.JoystickView;
+import com.zerokol.views.joystickView.JoystickView.*;
 
 import local.mahouse.rovercontroller.R;
 import local.mahouse.rovercontroller.Singleton;
 
 public class GalleryFragment extends Fragment {
 
+    private TextView mAngle, mPower, mDirection;
+    private int[] data = new int[4];
+    private JoystickView joystick;
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View galleryFragment = inflater.inflate(R.layout.fragment_gallery, container, false);//You f*ucking idiot
 
-        // -------------------------- DEPRECATED -------------------------
-        /*
-        //Inicialitzem els objectes en pantalla i l'adaptador per la llista
-        final Button send = galleryFragment.findViewById(R.id.btnSend);
-        final EditText message = galleryFragment.findViewById(R.id.editTextMessage);
-        final ListView list = galleryFragment.findViewById(R.id.listMessages);
+        mAngle = (TextView) galleryFragment.findViewById(R.id.txtAngle);
+        mPower = (TextView) galleryFragment.findViewById(R.id.txtPower);
+        mDirection = (TextView) galleryFragment.findViewById(R.id.txtDirection);
+        //JoystickView is a module created by @alvesoaj on Github, all rights reserved to him
+        //His github: https://github.com/alvesoaj/JoystickView
+        joystick = (JoystickView) galleryFragment.findViewById(R.id.joystickControll);
 
-        //Inicialitzem l'adaptador
-        ArrayList<String> strMessages = new ArrayList<String>();
-        ArrayAdapter<String> result = new ArrayAdapter<String>(galleryFragment.getContext(), android
-                .support
-                .v7
-                .appcompat
-                .R
-                .layout.support_simple_spinner_dropdown_item, strMessages);
-        list.setAdapter(result);
-        result.notifyDataSetChanged();
+        //Event listener that always returns the variation of the angle in degrees, motion power in percentage and direction of movement
+        joystick.setOnJoystickMoveListener(new OnJoystickMoveListener() {
 
-
-        new Thread(() -> {
-            while(true) {
-                //Listen for incoming messages
-                if(!Singleton.isStringBufferEmpty()) {
-                    getActivity().runOnUiThread(() -> {
-                        strMessages.addAll(Singleton.messages);
-                        result.notifyDataSetChanged();
-                        Singleton.messages.clear();
-                        Singleton.setStringBufferEmpty(true);
-                    });
-                }
-            }
-        }).start();
-
-        send.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onValueChanged(int angle, int power, int direction) {
                 if(Singleton.isConnected()) {
-                    if (!Singleton.sendIt(message.getText().toString())) {
-                        Toast.makeText(getContext(), getText(R.string.error_sending_message), Toast.LENGTH_LONG).show();
-                    } else {
-                        strMessages.add("[Me] " + message.getText().toString());
-                        result.notifyDataSetChanged();
+                    data[0] = 1; //We move
+
+                    mAngle.setText(" " + String.valueOf(angle) + "°");
+
+                    mPower.setText(" " + String.valueOf(power) + "%");
+
+                    //Let's Fucking Rock!
+                    switch (direction) {
+                        case JoystickView.FRONT:
+                            mDirection.setText(R.string.front_lab);
+                            break;
+                        case JoystickView.FRONT_RIGHT:
+                            mDirection.setText(R.string.front_right_lab);
+                            break;
+                        case JoystickView.RIGHT:
+                            mDirection.setText(R.string.right_lab);
+                            break;
+                        case JoystickView.RIGHT_BOTTOM:
+                            mDirection.setText(R.string.right_bottom_lab);
+                            break;
+                        case JoystickView.BOTTOM:
+                            mDirection.setText(R.string.bottom_lab);
+                            break;
+                        case JoystickView.BOTTOM_LEFT:
+                            mDirection.setText(R.string.bottom_left_lab);
+                            break;
+                        case JoystickView.LEFT:
+                            mDirection.setText(R.string.left_lab);
+                            break;
+                        case JoystickView.LEFT_FRONT:
+                            mDirection.setText(R.string.left_front_lab);
+                            break;
+                        default:
+                            mDirection.setText(R.string.center_lab);
+                            data[1] = 0;
+                            data[2] = 0;
+                            data[3] = 0;
                     }
+
+                    //Turns out java doesn't fucking have switch with a range of numbers, so if - else it is
+                    //Let's use the power of  M A T H
+
+                    //S'HA DE TENIR EN COMPTE QUE EL 0 MIRA CAP AMUNT
+                    if(0 < angle && angle < 45) { //Davant-dreta
+                        //Forward Right
+                        data[1] = 1;
+                        data[2] = power;
+                        data[3] = 100 * (int) Math.cos(angle * 2);
+                    } else if (45 <= angle && angle < 90) { //Dreta
+                        data[1] = 4;
+                        data[2] = power;
+                        data[3] = power * (int) Math.sin(angle * 2);
+
+                    } else if (90 <= angle && angle < 135) { //Dreta
+                        data[1] = 4;
+                        data[2] = power * (int)  Math.sin(angle * 2);
+                        data[3] = power;
+                    } else if (135 <= angle && angle <= 180) { //Dreta - avall
+                        data[1] = 2;
+                        data[2] = power * (int) - Math.cos(angle * 2);
+                        data[3] = power;
+                    } else if (-180 <= angle && angle < -135) {
+                        data[1] = 2;
+
+                        //TODO: Acabar procesament de l'angle i velocitat
+                    }
+
                 } else {
-                    Toast.makeText(getContext(), getText(R.string.error_not_connected), Toast.LENGTH_LONG).show();
+
                 }
             }
-        });
+        }, JoystickView.DEFAULT_LOOP_INTERVAL);
 
-         */
 
         return galleryFragment;
     }
